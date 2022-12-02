0d3faa359e1dc8ca2a69066ce45faef98f03c498
Yana Krukovets <yanashelli@gmail.com>
2022-12-02 15:44:33 -0500

    Add friends, movies, mutations

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,11 +1,38 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
+const _ = require("lodash");
 
 const resolvers = {
     Query: {
-        users() {
+        users: () => {
             return UserList; 
         },
+        user: (parent, args) => {
+            const id = args.id;
+            const user = _.find(UserList, { id: Number(id) });
+            return user;
+        },
+
+        movies: () => {
+            return MovieList;
+        },
+        movie: (parent, args) => {
+            const name = args.name;
+            const movie = _.find(MovieList, { name });
+            return movie;
+        },
     },
+    User: {
+        favoriteMovies: () => {
+            return _.filter(MovieList, (movie) => movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2022);
+        }
+    },
+
+    Mutation: {
+        createUser: (parent, args) => {
+            const user = args.input;
+            console.log(user);
+        }
+    }
 }
 
 module.exports  = { resolvers }
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -7,10 +7,41 @@ const typeDefs = gql`
         username: String!
         age: Int!
         nationality: String!
+        friends: [User]
+        favoriteMovies: [Movie]
     }    
+
+    type Movie {
+        id: ID!
+        name: String!
+        yearOfPublication: Int!
+        isInTheaters: Boolean!
+    }
 
     type Query {
         users: [User]!
+        user(id: ID!): User!
+        movies: [Movie!]!
+        movie(name: String!): Movie!
+    }
+
+    input CreateUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = BRAZIL
+    }
+
+    type Mutation {
+        createUser(input: CreateUserInput!): User!
+    }
+
+    enum Nationality {
+        CANADA
+        BRAZIL
+        INDIA
+        GERMANY
+        CHILE
     }
 `;
 
