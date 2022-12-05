3ee640f9dde6497a5fafeea505da8d7c7b52203b
Yana Krukovets <yanashelli@gmail.com>
2022-12-05 10:44:49 -0500

    Add user

--- client/src/DisplayData.js
+++ client/src/DisplayData.js
@@ -1,84 +1,162 @@
-import React, { useReducer, useState } from "react";
-import { useQuery, useLazyQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery, useLazyQuery, gql, useMutation } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
-    query GetAllUsers {
-        users {
-            id
-            name
-            age
-            username
-        }
+  query GetAllUsers {
+    users {
+      id
+      name
+      age
+      username
+      nationality
     }
+  }
 `;
 
-const QUERY_ALL_Movies = gql`
-    query GetAllMovies {
-        movies {
-            name
-        }
+const QUERY_ALL_MOVIES = gql`
+  query GetAllMovies {
+    movies {
+      id
+      name
     }
+  }
 `;
 
 const GET_MOVIE_BY_NAME = gql`
-    query GetMovieByNAme($name: String!) {
-        movie(name: $name) {
-            name
-            yearOfPublication
-        } 
+  query Movie($name: String!) {
+    movie(name: $name) {
+      name
+      yearOfPublication
     }
+  }
 `;
 
-function DisplayData () {
-    const [movieSearched, setMovieSearched] = useState("");
-
-    const { data, loading, error } = useQuery(QUERY_ALL_USERS);
-    const { data: movieData } = useQuery(QUERY_ALL_Movies);
-
-    const [fetchMovie, {data: movieSearchData}] = useLazyQuery(GET_MOVIE_BY_NAME);
-
-    if (loading) {
-        <h1>Data is loading...</h1>
+const CREATE_USER_MUTATION = gql`
+  mutation CreateUser($input: CreateUserInput!) {
+    createUser(input: $input) {
+      name
+      id
     }
+  }
+`;
 
-    if (error) {
-        console.log(error);
-    }
-    
-    return (
+function DisplayData() {
+  const [movieSearched, setMovieSearched] = useState("");
+
+  // Create User States
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState(0);
+  const [nationality, setNationality] = useState("");
+
+  const { data, loading, refetch } = useQuery(QUERY_ALL_USERS);
+  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+  const [
+    fetchMovie,
+    { data: movieSearchedData, error: movieError },
+  ] = useLazyQuery(GET_MOVIE_BY_NAME);
+
+  const [createUser] = useMutation(CREATE_USER_MUTATION);
+
+  if (loading) {
+    return <h1> DATA IS LOADING...</h1>;
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="number"
+          placeholder="Age..."
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Nationality..."
+          onChange={(event) => {
+            setNationality(event.target.value.toUpperCase());
+          }}
+        />
+        <button
+          onClick={() => {
+            createUser({
+              variables: {
+                input: { name, username, age: Number(age), nationality },
+              },
+            });
+
+            refetch();
+          }}
+        >
+          Create User
+        </button>
+      </div>
+      {data &&
+        data.users.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1>Name: {user.name}</h1>
+              <h1>Username: {user.username}</h1>
+              <h1>Age: {user.age}</h1>
+              <h1>Nationality: {user.nationality}</h1>
+            </div>
+          );
+        })}
+
+      {movieData &&
+        movieData.movies.map((movie) => {
+          return <h1 key={movie.id}>Movie Name: {movie.name}</h1>;
+        })}
+
+      <div>
+        <input
+          type="text"
+          placeholder="Interstellar..."
+          onChange={(event) => {
+            setMovieSearched(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            fetchMovie({
+              variables: {
+                name: movieSearched,
+              },
+            });
+          }}
+        >
+          Fetch Data
+        </button>
+        <div>
+          {movieSearchedData && (
             <div>
-                {data && 
-                    data.users.map((user) => {
-                        return (<div>
-                            <h1>Name: {user.name}</h1>
-                            <h2>UserName: {user.username}</h2>
-                            <h2>Age: {user.age}</h2>
-                        </div>);
-                })}
-
-                {movieData && movieData.movies.map((movie) => {
-                    return <h1>Movie Name: {movie.name}</h1>
-                })}
-
-                <div>
-                    <input type="text" placeholder="Interstellar..." onChange={(event) => {
-                        setMovieSearched(event.target.value);
-                        }}/>
-                    <button onClick={() => {
-                        fetchMovie({
-                            variables: {
-                                name: movieSearched,
-                            },
-                        })
-                        }}>Fetch Data</button>
-                    <div>
-                        {movieSearchData && <div>
-                                <h1>Movie name: {movieSearchData.movie.name}</h1>
-                                <h2>Date published: {movieSearchData.movie.yearOfPublication}</h2>
-                            </div>}
-                    </div>
-                </div>
-            </div>);
+              <h1>MovieName: {movieSearchedData.movie.name}</h1>
+              <h1>
+                Year Of Publication: {movieSearchedData.movie.yearOfPublication}
+              </h1>{" "}
+            </div>
+          )}
+          {movieError && <h1> There was an error fetching the data</h1>}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default DisplayData;
